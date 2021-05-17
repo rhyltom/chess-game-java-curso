8ec8118ab50beec774dd073cad60056da6cedb63
Emanuel <dendiiiman@gmail.com>
2021-05-17 15:22:24 +0100

    moving pieces

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,14 +1,31 @@
 package app;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 
-		ChessMatch match = new ChessMatch();
-		UI.printBoard(match.getPieces());
+		Scanner sc = new Scanner(System.in);
+		ChessMatch chessMatch = new ChessMatch();
+		
+		while(true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.println("source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			System.out.println();
+			System.out.println("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
+			
 	}
 
 }
--- src/app/UI.java
+++ src/app/UI.java
@@ -1,6 +1,10 @@
 package app;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
@@ -35,6 +39,18 @@ public class UI {
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
+	}
+
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		}
+		catch (RuntimeException e) {
+			throw new InputMismatchException("erro lendo posição de xadrez!!");
+		}
 	}
 
 	private static void printPiece(ChessPiece piece) {
